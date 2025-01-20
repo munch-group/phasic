4d0f2d545c6f0ec6e7634126ee7aa819bd826490
Kasper Munch <kaspermunch@birc.au.dk>
2025-01-20 08:57:44 +0100

    update

--- src/cpp/ptdalgorithmscpp_pybind.cpp
+++ src/cpp/ptdalgorithmscpp_pybind.cpp
@@ -25,18 +25,16 @@ PYBIND11_MODULE(ptdalgorithmscpp_pybind, m) {
   .def(py::init<struct ::ptd_graph* >(), py::arg("ptd_graph"))
   .def(py::init<struct ::ptd_graph*, struct ::ptd_avl_tree* >(), py::arg("ptd_graph"), py::arg("ptd_avl_tree"))
   .def(py::init<const ptdalgorithms::Graph>(), py::arg("o"))
-//   .def("update_weights_parameterized", &ptdalgorithms::Graph::update_weights_parameterized, py::arg("rewards") = std::vector<double>{})
   .def("update_weights_parameterized", &ptdalgorithms::Graph::update_weights_parameterized, py::arg("rewards") = std::vector<double>())
   .def("expected_waiting_time", &ptdalgorithms::Graph::expected_waiting_time, py::arg("rewards") = std::vector<double>())
   .def("expected_residence_time", &ptdalgorithms::Graph::expected_residence_time, py::arg("rewards") = std::vector<double>())
-  .def("create_vertex", &ptdalgorithms::Graph::create_vertex, py::arg("state") = std::vector<int>())
-  .def("find_vertex", &ptdalgorithms::Graph::find_vertex, py::arg("state"))
+//   .def("create_vertex", &ptdalgorithms::Graph::create_vertex, py::arg("state"))
+//   .def("find_vertex", &ptdalgorithms::Graph::find_vertex, py::arg("state"))
 //   .def("vertex_exists", &ptdalgorithms::Graph::vertex_exists, py::arg("state"))
 //   .def("find_or_create_vertex", &ptdalgorithms::Graph::find_or_create_vertex, py::arg("state"))
   .def("starting_vertex", &ptdalgorithms::Graph::starting_vertex)
   .def("vertices", &ptdalgorithms::Graph::vertices)
   .def("vertex_at", &ptdalgorithms::Graph::vertex_at, py::arg("index"))
-
   .def("vertices_length", &ptdalgorithms::Graph::vertices_length)
   .def("__repr__",
     [](ptdalgorithms::Graph &g) {
@@ -46,6 +44,18 @@ PYBIND11_MODULE(ptdalgorithmscpp_pybind, m) {
 
 
     ;
+  py::class_<ptdalgorithms::Vertex>(m, "Vertex")
+  .def(py::init<int>(), py::arg("state_length"))
+
+        Vertex(Graph &graph, int *state) : graph(graph) {
+            this->vertex = ptd_vertex_create_state(graph.rf_graph->graph, state);
+
+            if (this->vertex == NULL) {
+                throw std::runtime_error("Failed to create ptd_vertex\n");
+            }
+        }
+
+
 }
 
 
