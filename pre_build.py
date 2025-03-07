f62fd254b1d0857e846d5f5092f99ae35d20c24f
Kasper Munch <kaspermunch@birc.au.dk>
2025-03-07 17:53:17 -0500

    update

--- pre_build.py
+++ pre_build.py
@@ -1,19 +1,25 @@
 import os
 import sys
 
-def symlink_eigen(prefix):
-  target_path = f"{prefix}/include/eigen3/Eigen"
-  link_path = f"{prefix}/include/Eigen" 
-  try:
-      os.symlink(target_path, link_path)
-  except FileExistsError:
-      pass
+def fix_eigen():
 
-if "BUILD_PREFIX" in os.environ and os.environ["BUILD_PREFIX"]:
-    symlink_eigen(os.environ["BUILD_PREFIX"])
+    def symlink_eigen(prefix):
+        target_path = f"{prefix}/include/eigen3/Eigen"
+        link_path = f"{prefix}/include/Eigen" 
+        try:
+            os.symlink(target_path, link_path)
+        except FileExistsError:
+            pass
 
-if "PREFIX" in os.environ and os.environ["PREFIX"]:
-    symlink_eigen(os.environ["PREFIX"])
+        if "BUILD_PREFIX" in os.environ and os.environ["BUILD_PREFIX"]:
+            symlink_eigen(os.environ["BUILD_PREFIX"])
 
-symlink_eigen(sys.exec_prefix)
+        if "PREFIX" in os.environ and os.environ["PREFIX"]:
+            symlink_eigen(os.environ["PREFIX"])
+
+        symlink_eigen(sys.exec_prefix)
+
+if __name__ == "__main__":
+    fix_eigen()
+
 
--- setup.py
+++ setup.py
@@ -5,31 +5,26 @@ import platform
 import subprocess
 from setuptools import setup, find_packages
 
-version = "0.1.20"
+version = "0.1.21"
 
-# if "BUILD_PREFIX" in os.environ and os.environ["BUILD_PREFIX"]:
-#     prefix = os.environ["BUILD_PREFIX"]
-# if "PREFIX" in os.environ and os.environ["PREFIX"]:
+# if 'PREFIX' in os.environ and os.environ["PREFIX"]:
 #     prefix = os.environ["PREFIX"]
-# # else:
-# #     prefix = sys.exec_prefix
+# else:
+#     prefix = sys.exec_prefix
+prefix = sys.exec_prefix
 
-if 'PREFIX' in os.environ and os.environ["PREFIX"]:
-    prefix = os.environ["PREFIX"]
-else:
-    prefix = sys.exec_prefix
+from pybind11.setup_helpers import Pybind11Extension, ParallelCompile, build_ext, naive_recompile
+# from pybind11.setup_helpers import Pybind11Extension, ParallelCompile, naive_recompile
+# from setuptools.command.build_ext import build_ext as _build_ext
+# class build_ext(_build_ext):
+#     def run(self):
+#         # # Run the pre-build command
+#         pre_build_command = "python pre_build.py"
+#         subprocess.check_call(pre_build_command, shell=True)
+#         assert 0
 
-# from pybind11.setup_helpers import Pybind11Extension, ParallelCompile, build_ext, naive_recompile
-from pybind11.setup_helpers import Pybind11Extension, ParallelCompile, naive_recompile
-from setuptools.command.build_ext import build_ext as _build_ext
-class build_ext(_build_ext):
-    def run(self):
-        # Run the pre-build command
-        pre_build_command = "python pre_build.py"
-        subprocess.check_call(pre_build_command, shell=True)
-
-        # Continue with the normal build process
-        _build_ext.run(self)
+#         # Continue with the normal build process
+#         _build_ext.run(self)
 
 # Optional multithreaded build
 ParallelCompile("NPY_NUM_BUILD_JOBS").install()
@@ -43,7 +38,7 @@ extra_link_args = ["-g", f"-I{prefix}/include/eigen3/"]
 if platform.system() == "Darwin":
   # Compiling on macOS requires an installation of the Xcode Command Line Tools
   os.environ["CC"] = "g++"
-  os.environ["CXX"] = "g++"
+  os.environ["CXX"] = "g++ -std=c++11"
   # extra_link_args.extend(["-undefined", "dynamic_lookup"])
 
 #include/eigen3
@@ -69,37 +64,13 @@ setup(name='ptdalgorithms',
       author_email='',
       url='',
       packages = find_packages(),
-    #   package_dir = {'SAP': 'SAP'},
-    #   include_package_data = True,     
       extras_require={"test": "pytest"},
       cmdclass={"build_ext": build_ext}, 
       ext_modules=ext_modules,
-      # package_data={
-      # 'ptdalgorithms.data.src.c': ['src/c/*'],
-      # 'ptdalgorithms.data.src.cpp': ['src/cpp/*'],
-      # 'ptdalgorithms.data.api.c': ['api/c/*'],
-      # 'ptdalgorithms.data.api.cpp': ['api/cpp/*'],
-      #  },
       data_files=[
         ('include', ['ptdalgorithms/ptdalgorithms.h']),
         ('include/ptdalgorithms/api/c', ['api/c/ptdalgorithms.h']),
         ('include/ptdalgorithms/api/cpp', ['api/cpp/ptdalgorithmscpp.h']),
         ('include/ptdalgorithms/src/c', ['src/c/ptdalgorithms.c', 'src/c/ptdalgorithms.h']),
         ('include/ptdalgorithms/src/cpp', ['src/cpp/ptdalgorithmscpp.cpp', 'src/cpp/ptdalgorithmscpp.h', 'src/cpp/ptdalgorithmscpp_pybind.cpp']),
-
-        # ('include/ptdalgorithms/src/c', ['src/c/ptdalgorithms.c', 'src/c/ptdalgorithms.h']),
-        # ('include/ptdalgorithms/src/cpp', ['src/cpp/ptdalgorithmscpp.cpp', 'src/cpp/ptdalgorithmscpp.h']),
-
-        # ('include', ['ptdalgorithms/ptdalgorithms.h']),
-        # ('include/ptdalgorithms/api/c', glob('api/c/*.h`') + glob('api/c/*.c`')),
-        # ('include/ptdalgorithms/api/cpp', glob('api/cpp/*.h`') + glob('api/cpp/*.cpp`')),
-        # ('include/ptdalgorithms/src/c', glob('src/c/*.h`') + glob('src/c/*.c`')),
-        # ('include/ptdalgorithms/src/cpp', glob('src/cpp/*.h`') + glob('src/cpp/*.cpp`')),
-        # ('include/api', [f for f in glob('src/**', recursive=True) if os.path.isfile(f)]),
-        # ('include/src', [f for f in glob('api/**', recursive=True) if os.path.isfile(f)]),
-                  ],
-     )
-#include "../api/c/ptdalgorithms.h"
-
-#include "../src/c/ptdalgorithms.c"
-#include "../api/cpp/ptdalgorithmscpp.h"
+      ])
