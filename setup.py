1a950a837c4415e9035aff2298137b5a4ade4adc
Kasper Munch <kaspermunch@birc.au.dk>
2025-03-07 22:46:23 -0500

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import platform
 import subprocess
 from setuptools import setup, find_packages
 
-version = "0.1.32"
+version = "0.1.33"
 
 if 'PREFIX' in os.environ and os.environ["PREFIX"]:
     prefix = os.environ["PREFIX"]
