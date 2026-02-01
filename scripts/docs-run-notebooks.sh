4eaf89d7003851c98a6d30869035a46e1e75676d
Kasper Munch <kaspermunch@birc.au.dk>
2026-02-01 23:29:14 +0100

    Docs update

--- scripts/docs-run-notebooks.sh
+++ scripts/docs-run-notebooks.sh
@@ -6,8 +6,12 @@ NC='\033[0m' # No Color
 cd docs
 DIR=pages
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if test -d $DIR; then
-  for FILE in $(find $DIR -name '*.ipynb') ; do
+  for FILE in $(find $DIR -name '*.ipynb' | sort) ; do
     if [[  "$(grep $FILE _quarto.yml | grep -v '#')" ]]; then
       echo -e "${BLUE}Rendering ${FILE}${NC}"
       # CMD="jupyter nbconvert --Application.log_level=50 --to notebook --execute --inplace $FILE"
@@ -15,7 +19,14 @@ if test -d $DIR; then
         --TagRemovePreprocessor.enabled=True \
         --TagRemovePreprocessor.remove_cell_tags='{"skip-execution"}' $FILE"
       echo $CMD
-      NOTEBOOK_THEME=light PYDEVD_DISABLE_FILE_VALIDATION=1 $CMD || exit 1 ;
+    #  NOTEBOOK_THEME=light PYDEVD_DISABLE_FILE_VALIDATION=1 $CMD || exit 1 ;
+     NOTEBOOK_THEME=light PYDEVD_DISABLE_FILE_VALIDATION=1 $CMD > /dev/null 2>&1 && {
+      echo -e "${GREEN}Success${FILE}${NC}"
+     } || {
+        echo -e "${RED}Error${FILE}${NC}"
+        errors=true
+      }
+      echo
     fi
   done
 else
@@ -27,3 +38,8 @@ for FILE in $(find $DIR -type f -size +50M); do
   echo "$FILE too large. Clearing outputs."
   jupyter nbconvert --clear-output --inplace $FILE
 done
+
+if [ "$errors" = true ] ; then
+  echo "One or more notebooks failed to execute"
+  exit 1
+fi
